b27f588efe108ffcebc6002c5c82761909ce9ffc
igor-sharipov1 <70577702+igor-sharipov1@users.noreply.github.com>
2023-10-01 20:48:21 +0500

    1 lab deep learning

---
+++ program.py
@@ -0,0 +1,12 @@
+import os
+
+os.chdir('C:\\NeuralNetwork\\Network1')
+
+import mnist_loader
+
+training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
+
+import network
+
+net = network.Network([784, 30, 10])
+net.SGD(training_data, 30, 10, 1, test_data=test_data)
